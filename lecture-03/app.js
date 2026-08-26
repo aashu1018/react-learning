c0a1e5d2be62b6d64efad0eb9895b13bf024650d
Ashutosh Singh <ashu.ksingh1018@gmail.com>
2026-08-26 19:34:58 +0530

    practice

--- lecture-03/app.js
+++ lecture-03/app.js
@@ -2,6 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //React element(a JavaScript object that represents a DOM element.)
+// const Title = () => (
+//     <h1 className='head' tabIndex="5">
+//     Laying the foundation using JSX!
+//     </h1>);
+
+//Component composition
+// const HeadingComponent = () => (
+//     <div id="container">
+//         <Title />
+//         <h1>Functional component</h1>
+//     </div>
+// );
+
+// const number = 1000;
+
+//Any JS code can be written inside a curly braces and put inside the JSX function
+// const HeadingComponent = () => (
+//     <div id="container">
+//         {number}
+//         <h1>Functional component</h1>
+//     </div>
+// );
+
+// const elem = <span>React Element</span>
+
+// const HeadingComponent = () => (
+//     <div id="container">
+//         <h1>Functional component</h1>
+//     </div>
+// );
+
+// const Title = () => (
+//     <h1 className='head' tabIndex="5">
+//         {elem}
+//     Laying the foundation using JSX!
+//     <HeadingComponent />
+//     </h1>);
+
 const Title = () => (
     <h1 className='head' tabIndex="5">
     Laying the foundation using JSX!
@@ -9,8 +47,10 @@ const Title = () => (
 
 const HeadingComponent = () => (
     <div id="container">
+        {Title()}
         <Title />
-        <h1>Functional component</h1>
+        <Title></Title>
+        <h1 className="heading">Functional component</h1>
     </div>
 );
 
