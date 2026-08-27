2d253cb766cb6191b108219c6be2905ae6470a02
Ashutosh Singh <ashu.ksingh1018@gmail.com>
2026-08-27 22:59:05 +0530

    header

--- food-ordering-app/app.js
+++ food-ordering-app/app.js
@@ -1,2 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import logo from 'url:./Logo.png';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={logo} alt="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-contsiner">
+
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
